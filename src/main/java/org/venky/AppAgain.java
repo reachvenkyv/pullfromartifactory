9d25832e54d3ecc5b6b5111f954fd59416ba974b
reachvenkyv <reachvenkyv@gmail.com>
2019-07-26 16:28:26 +1000

    First commit

---
+++ src/main/java/org/venky/AppAgain.java
@@ -0,0 +1,16 @@
+package org.venky;
+
+import mygroup.parent.App;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppAgain 
+{
+    public static void main( String[] args )
+    {
+    	new App();
+        System.out.println( "Hello World in AppAgain!" );
+    }
+}
